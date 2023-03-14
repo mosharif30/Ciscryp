9f7b1dcd7ce7a84244212879571b21b16a5670b4
mosharif30 <sharifkhani.mr@gmail.com>
2023-03-15 02:34:17 +0330

    dark mode header bug fix

--- src/components/Header/HeaderLogged.tsx
+++ src/components/Header/HeaderLogged.tsx
@@ -5,7 +5,7 @@ export interface HeaderLoggedProps {}
 
 const HeaderLogged: FC<HeaderLoggedProps> = () => {
   return (
-    <div className="nc-HeaderLogged sticky top-0 w-full z-40 bg-white">
+    <div className="nc-HeaderLogged sticky top-0 w-full z-40 bg-white dark:bg-neutral-900">
       {/* NAV */}
       <MainNav2Logged />
     </div>
